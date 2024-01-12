4b164cc20c14759d8dbab34f3228da3c59513de7
vinicius_silva2002 <viniciussoaresaqw@hotmail.com>
2024-01-11 23:03:24 -0300

    Gilbert

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,7 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
-      <body className="bg-gradient-to-tr from-white via-white to-cyan-100 text-purple-700">
+      <body className={`bg-gradient-to-tr from-white via-white to-cyan-100 text-purple-700 ${inter.className}`}>
         <Header />
         <main className="min-h-[70vh] my-4">{children}</main>
         <div className="fixed bottom-5 right-5 md:bottom-32 md:right-32 z-10 flex">
